8ca1b003130212dc762ff639bea157a9e7e6de0e
Cenez Araujo de Rezende <cenezaraujo@gmail.com>
2025-09-20 01:10:24 -0300

    .

---
+++ cluster_environment_install/Linux/3-mono_mpi.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "################## Para instalar Mono e MPI #######################################"
+echo "Solicitar binários 'mono_mpi_environment.tar.gz' no grupo!"
+echo "Siga as instruções em:"
+echo "   https://github.com/UFC-Jaguar/jaguar-lang/tree/main/cluster_environment_install"
+
