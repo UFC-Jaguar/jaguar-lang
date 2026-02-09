3eb7afd024a3d8776b985231fc67e2b21a3da36a
Cenez Araujo de Rezende <cenezaraujo@gmail.com>
2026-02-09 18:16:01 -0300

    Refactoring GRAMMAR IF ELIF ELSE

--- Base/Jaguar/Common/VisitorNodes/NoIF.cs
+++ Base/Jaguar/Common/VisitorNodes/NoIF.cs
@@ -2,52 +2,43 @@
 using System.Collections.Generic;
 using Common.Data;
 
-/* TODO: NoIF precisa ser refatorado. O ideal é que tenhamos algo assim, usando visitor para cada noVisitor:
-  <IfExp> ::= KEY:IF <Exp> KEY:DO (<Stm> [(<Elif> | <Else>)]
-                                   | NEWLINE <Statements> (KEY:END | <Elif> | <Else>)
-                                  )
-  <Elif>  ::= KEY:ELIF <Exp> KEY:DO (<Stm> [(<Elif> | <Else>)]
-                                     | NEWLINE <Statements> (KEY:END | <Elif> | <Else>)
-                                    )
-  <Else>  ::= KEY:ELSE (<Stm>
-                        | (NEWLINE <Statements> KEY:END)
-                       )
-  Note que a primeira opção é inline. A segunda envolve NEWLINE
-*/
 namespace Common.Nodes {
     public class NoIF: Visitor {
-        private readonly List<NoDataIFs> __QIFs = null;
+        private readonly List<NoDataIFs> __Cases = null;
         private readonly NoElse __ELSE = null;
 
-        public List<NoDataIFs> QueueIFs { get { return __QIFs; } }
+        public List<NoDataIFs> IFCases { get { return __Cases; } }
         public NoElse Else { get { return __ELSE; } }
         public NoIF(List<NoDataIFs> _queueIfs, NoElse _else){ // TODO: este construtor pode ser melhorado?
-            this.__QIFs = _queueIfs;
+            this.__Cases = _queueIfs;
             this.__ELSE = _else;
-            if (this.QueueIFs.Count > 0) {
-                this.NOIni = this.QueueIFs[0].Item1.NOIni;
-                this.NOEnd = this.QueueIFs[this.QueueIFs.Count - 1].Item1.NOEnd;
+            if (this.IFCases.Count > 0) {
+                this.NOIni = this.IFCases[0].Condition.NOIni;
+                this.NOEnd = this.IFCases[this.IFCases.Count - 1].Condition.NOEnd;
             }
             if (this.Else != null)
-                this.NOEnd = this.Else.Item1.NOEnd;
+                this.NOEnd = this.Else.Body.NOEnd;
         }
         public override string ToString() {
             string sb = "["; string space = ", "; int c = 0;
-            foreach (var tp in QueueIFs) {
-                sb = sb+"("+tp.Item1.ToString()+", "+tp.Item2.ToString()+")";
-                sb = ++c == QueueIFs.Count ? sb : sb + space;
+            foreach (var tp in IFCases) {
+                sb = sb+"("+tp.Condition.ToString()+", "+tp.Body.ToString()+")";
+                sb = ++c == IFCases.Count ? sb : sb + space;
             }
             sb = sb + "]";
             return sb;
         }
+        
         public static NoElse ElseInstance(Visitor n, bool b) { return new NoElse(n, b); }
+        
         public static NoDataIFs DataIFInstance(Visitor n1, Visitor n2, bool b) { return new NoDataIFs(n1, n2, b); }
+        
         public override MemoryManager Visit(JMemory memory) {
             MemoryManager manager = new MemoryManager();
-            foreach (var tp in this.QueueIFs) {
-                Visitor condition = tp.Item1;
-                Visitor exp = tp.Item2;
-                bool needReturnNull = tp.Item3;
+            foreach (var tp in this.IFCases) {
+                Visitor condition = tp.Condition;
+                Visitor exp = tp.Body;
+                bool needReturnNull = tp.NeedReturn;
                 TValue conditionValue = manager.Registry(condition.Visit(memory)); //TODO: Ver isso
                 if (manager.NeedReturn) 
                     return manager;
@@ -61,8 +52,8 @@ namespace Common.Nodes {
                 }
             }
             if (this.Else != null) {
-                Visitor exp = this.Else.Item1;
-                bool needReturnNull = this.Else.Item2;
+                Visitor exp = this.Else.Body;
+                bool needReturnNull = this.Else.NeedReturn;
                 TValue expValue = manager.Registry(exp.Visit(memory));
                 if (manager.NeedReturn) 
                     return manager;
@@ -73,29 +64,23 @@ namespace Common.Nodes {
         }
         //################################################ VAL ELSE:
         public class NoElse : Visitor {
-            private Tuple<Visitor, bool> __VISITOR_TUPLE = null;
-            public Tuple<Visitor, bool> VisitorTuple { get { return __VISITOR_TUPLE; } }
-            public Visitor Item1 { get { return VisitorTuple.Item1; } }
-            public bool Item2 { get { return VisitorTuple.Item2; } }
-            public NoElse(Visitor node, bool ret) {
-                __VISITOR_TUPLE = new Tuple<Visitor, bool>(node, ret);
+            public Visitor Body { get; set; } 
+            public bool NeedReturn { get; set; } 
+
+            public NoElse(Visitor body, bool need_return) {
+                this.Body = body;
+                this.NeedReturn = need_return;
             }
+
             public override MemoryManager Visit(JMemory memory) { // TODO: Ver isso. Podemos tratar algo aqui no visit?
                 throw new NotImplementedException();
             }
         }
         //################################################ VAL ELIF:
-        public class NoDataIFs : Visitor {
-            private Tuple<Visitor, Visitor, bool> __VISITOR_TUPLE = null;
-            
-            public Visitor Item1 { get { return __VISITOR_TUPLE.Item1; } }
-            public Visitor Item2 { get { return __VISITOR_TUPLE.Item2; } }
-            public bool Item3 { get { return __VISITOR_TUPLE.Item3; } }
-            public NoDataIFs(Visitor no1, Visitor no2, bool ret) {
-                __VISITOR_TUPLE = new Tuple<Visitor, Visitor, bool>(no1, no2, ret);
-            }
-            public override MemoryManager Visit(JMemory memory) { // TODO: Ver isso. Podemos tratar algo aqui no visit?
-                throw new NotImplementedException();
+        public class NoDataIFs : NoElse { 
+            public Visitor Condition { get; set; } 
+            public NoDataIFs(Visitor condition, Visitor body, bool need_return):base(body, need_return) { 
+                this.Condition = condition;
             }
         }
     }
--- Base/Jaguar/FrontEnd/Grammar/IfExp.cs
+++ Base/Jaguar/FrontEnd/Grammar/IfExp.cs
@@ -10,13 +10,13 @@ namespace FrontEnd.Grammar {
             AstInfo ast = new AstInfo();
             Node ifNode = ast.Registry(this.IFNODE(parser, Consts.KEYS[Consts.IDX.IF]));
             if (ast.Error != null) return ast;
-            var cases = ((NoIF)ifNode).QueueIFs;
+            var cases = ((NoIF)ifNode).IFCases;
             var elseStatement = ((NoIF)ifNode).Else;
             return ast.Success(new NoIF(cases, elseStatement));
         }
-        private AstInfo IFNODE(Parser parser, string keyIF_ELIF) {// Default is Inline. IF First
+        private AstInfo IFNODE(Parser parser, string keyIF_ELIF) {
             AstInfo ast = new AstInfo();
-            var dataIfs = new List<NoIF.NoDataIFs>();
+            List<NoIF.NoDataIFs> dataIfs = new List<NoIF.NoDataIFs>();
             NoIF.NoElse noElse = null;
             if (!parser.Current.Matches(Consts.KEY, keyIF_ELIF)) {
                 return ast.Fail(new TError(
@@ -36,93 +36,75 @@ namespace FrontEnd.Grammar {
             }
             parser.NextToken(ast);
 
-            if (parser.Current.Type == Consts.NEWLINE) { // If new line
+            //
+            bool needReturnNull = true;
+            Visitor statements = ast.Registry((new Statements()).Rule(parser));
+            if (ast.Error != null)
+                return ast;
+            dataIfs.Add(NoIF.DataIFInstance(condition, statements, needReturnNull));
+            //
+
+            if (parser.Current.Matches(Consts.KEY, Consts.KEYS[Consts.IDX.END])) {
                 parser.NextToken(ast);
-
-                if (!NewCondition(ref ast, condition, new Statements(), dataIfs, true, parser)) 
-                    return ast;
-
-                if (parser.Current.Matches(Consts.KEY, Consts.KEYS[Consts.IDX.END])) {
-                    parser.NextToken(ast);
-                    return ast.Success(new NoIF(dataIfs, noElse));
-                }
-                if (!More_IFs_Else(ref ast, dataIfs, ref noElse, parser)) 
-                    return ast;
-                return ast.Success(new NoIF(dataIfs, noElse));
-            } // Inline:
-            if (!NewCondition(ref ast, condition, new Stm(), dataIfs, false, parser)) 
-                return ast;
-
-            if (!More_IFs_Else(ref ast, dataIfs, ref noElse, parser)) 
-                return ast;
-
-            return ast.Success(new NoIF(dataIfs, noElse));
-        }
-        private AstInfo ElifOrElse(Parser parser) {// Default is __ELSE
-            AstInfo ast = new AstInfo();
-            var dataIfs = new List<NoIF.NoDataIFs>();
-            NoIF.NoElse noElse;
-
-            if (parser.Current.Matches(Consts.KEY, Consts.KEYS[Consts.IDX.ELIF])) {
-                Node noIF = ast.Registry(this.IFNODE(parser, Consts.KEYS[Consts.IDX.ELIF]));
-                if (ast.Error != null) return ast;
-                dataIfs = ((NoIF)noIF).QueueIFs;
-                noElse = ((NoIF)noIF).Else;
                 return ast.Success(new NoIF(dataIfs, noElse));
             }
-            Node no = ast.Registry(this.ElseExp(parser));
+            // Case ELIF_Else
+            if (parser.Current.Matches(Consts.KEY, Consts.KEYS[Consts.IDX.ELIF])) { // elif
+                Node noIF = ast.Registry(this.Call_Elif(parser));
+                if (ast.Error != null)
+                    return ast;
+                dataIfs.AddRange(((NoIF)noIF).IFCases);
+                return ast.Success(new NoIF(dataIfs, ((NoIF)noIF).Else));
+            }
+            if (parser.Current.Matches(Consts.KEY, Consts.KEYS[Consts.IDX.ELSE])) { // else
+                Node noIF = ast.Registry(this.Call_Else(parser));
+                if (ast.Error != null)
+                    return ast;
+                dataIfs.AddRange(((NoIF)noIF).IFCases);
+                return ast.Success(new NoIF(dataIfs, ((NoIF)noIF).Else));
+            }
+            return ast.Fail(new TError( // Falha
+                parser.Current.NOIni, parser.Current.NOEnd, TError.ESyntax,
+                "Expected '" + Consts.KEYS[Consts.IDX.IF] + "'" + Consts.KEYS[Consts.IDX.ELIF]+"' or " + Consts.KEYS[Consts.IDX.ELSE]
+            ));
+        }
+        private AstInfo Call_Elif(Parser parser) {
+            AstInfo ast = new AstInfo();
+            NoIF.NoElse noElse;
+
+            Node noIF = ast.Registry(this.IFNODE(parser, Consts.KEYS[Consts.IDX.ELIF]));
             if (ast.Error != null) return ast;
-            noElse = (no != null && no.GetType() == typeof(NoIF.NoElse)) ? (NoIF.NoElse)no : null;
+            var dataIfs = ((NoIF)noIF).IFCases;
+            noElse = ((NoIF)noIF).Else;
             return ast.Success(new NoIF(dataIfs, noElse));
         }
-        private bool More_IFs_Else(ref AstInfo ast, List<NoIF.NoDataIFs> dataIFs, ref NoIF.NoElse noElse, Parser parser) {
-            Node noIF = ast.Registry(this.ElifOrElse(parser));
-            if (ast.Error != null) return false;
-            List<NoIF.NoDataIFs> newDataIF = ((NoIF)noIF).QueueIFs;
-            noElse = ((NoIF)noIF).Else;
-            dataIFs.AddRange(newDataIF);
-            return true;
-        }
-        private bool NewCondition(ref AstInfo ast, 
-                                      Visitor condition, Grammar grammar, 
-                                      List<NoIF.NoDataIFs> dataIFs, 
-                                      bool needReturnNull, Parser parser) {
-
-            Visitor expOrStatements = ast.Registry(grammar.Rule(parser));// TODO: Dá para melhorar ????
-            if (ast.Error != null) 
-                return false;
-            dataIFs.Add(NoIF.DataIFInstance(condition, expOrStatements, needReturnNull));
-            return true;
-        }
-        private AstInfo ElseExp(Parser parser) { // TODO: avaliar
+        private AstInfo Call_Else(Parser parser) {
             AstInfo ast = new AstInfo();
+            var dataIfs = new List<NoIF.NoDataIFs>();
             NoIF.NoElse noElse = null;
 
             if (parser.Current.Matches(Consts.KEY, Consts.KEYS[Consts.IDX.ELSE])) {
                 parser.NextToken(ast);
 
-                if (parser.Current.Type == Consts.NEWLINE) { // New line
+                Visitor statements = ast.Registry(new Statements().Rule(parser));
+                if (ast.Error != null)
+                    return ast;
+                noElse = NoIF.ElseInstance(statements, true);
+
+                if (parser.Current.Matches(Consts.KEY, Consts.KEYS[Consts.IDX.END])) {
                     parser.NextToken(ast);
-
-                    Visitor statements = ast.Registry(new Statements().Rule(parser));
-                    if (ast.Error != null) return ast;
-                    noElse = NoIF.ElseInstance(statements, true);
-
-                    if (parser.Current.Matches(Consts.KEY, Consts.KEYS[Consts.IDX.END])) {
-                        parser.NextToken(ast);
-                    } else {
-                        return ast.Fail(new TError(
-                          parser.Current.NOIni, parser.Current.NOEnd, TError.ESyntax,
-                          "Expected '" + Consts.KEYS[Consts.IDX.END] + "'"
-                        ));
-                    }
-                } else { // In line
-                    Visitor stm = ast.Registry(new Stm().Rule(parser));
-                    if (ast.Error != null) return ast;
-                    noElse = NoIF.ElseInstance(stm, false);
+                } else {
+                    return ast.Fail(new TError(
+                        parser.Current.NOIni, parser.Current.NOEnd, TError.ESyntax,
+                        "Expected '" + Consts.KEYS[Consts.IDX.END] + "'"
+                    ));
                 }
             }
-            return ast.Success(noElse);
+            Visitor no = ast.Registry(ast.Success(noElse));
+            if (ast.Error != null)
+                return ast;
+            noElse = (no != null && no.GetType() == typeof(NoIF.NoElse)) ? (NoIF.NoElse)no : null;
+            return ast.Success(new NoIF(dataIfs, noElse));
         }
     }
 }
